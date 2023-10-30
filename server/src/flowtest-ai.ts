3b188fdae7c5db2c0a604ffca18419ec2fde5ef6
Sajal Jain <sjain@Sajals-MacBook-Pro.local>
2023-10-30 14:27:27 -0700

    ft-63: use openapi to translate natural language instructions to openapi actions

--- server/src/flowtest-ai.ts
+++ server/src/flowtest-ai.ts
@@ -1,9 +1,26 @@
 import SwaggerParser from '@apidevtools/swagger-parser';
 import JsonRefs from 'json-refs'
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const SYSTEM_MESSAGE = `You are a helpful assistant. \ 
+        Respond to the following prompt by using function_call and then summarize actions. \ 
+        Ask for clarification if a user request is ambiguous.`
+
+// Maximum number of function calls allowed to prevent infinite or lengthy loops
+const MAX_CALLS = 5
 
 class FlowtestAI {
+
     async generate(user_instruction: string) {
-        await this.get_available_functions()
+        const functions = await this.get_available_functions();
+        await this.process_user_instruction(functions, user_instruction);
     }
 
     async get_available_functions() {
@@ -52,10 +69,62 @@ class FlowtestAI {
                 functions.push(
                     {"name": function_name, "description": desc, "parameters": schema}
                 )
-
-                console.log(JSON.stringify(functions));
             })
         })
+        console.log(JSON.stringify(functions));
+        return functions;
+    }
+
+    async get_openai_response(functions: any[], messages: any[]) {
+        return await openai.chat.completions.create({
+            model: "gpt-3.5-turbo-16k-0613",
+            functions: functions,
+            function_call: "auto",  // "auto" means the model can pick between generating a message or calling a function.
+            temperature: 0,
+            messages: messages,
+        });
+    }
+
+    async process_user_instruction(functions: any[], instruction: string) {
+        let result = []
+        let num_calls = 0
+        const messages: any[] = [
+            {"content": SYSTEM_MESSAGE, "role": "system"},
+            {"content": instruction, "role": "user"},
+        ];
+
+        while (num_calls < MAX_CALLS) {
+            const response = await this.get_openai_response(functions, messages)
+            // console.log(response)
+            const message = response["choices"][0]["message"]
+
+            if (message["function_call"]) {
+                console.log('Function call #: ', num_calls + 1)
+                console.log(message["function_call"])
+                messages.push(message)
+
+                // For the sake of this example, we'll simply add a message to simulate success.
+                // Normally, you'd want to call the function here, and append the results to messages.
+                messages.push(
+                    {
+                        "role": "function",
+                        "content": "success",
+                        "name": message["function_call"]["name"]
+                    }
+                )
+                result.push(message["function_call"])
+
+                num_calls += 1
+            } else {
+                console.log("Message: ")
+                console.log(message["content"])
+                break;
+            }
+        }
+
+        if (num_calls >= MAX_CALLS) {
+            console.log('Reached max chained function calls: ', MAX_CALLS)
+        }
     }
 }
 
--- server/tests/flowtest-ai.test.ts
+++ server/tests/flowtest-ai.test.ts
@@ -4,7 +4,10 @@ import FlowtestAI from "../src/flowtest-ai";
 describe("generate", () => {
     it("should generate functions from openapi spec", async () => {
         const f = new FlowtestAI();
-        let result = await f.generate("");
+        const USER_INSTRUCTION = 'Instruction: Add a new pet to the store. \
+            Then get the created pet. \
+            Then get pet with status as available.';
+        let result = await f.generate(USER_INSTRUCTION);
         //expect(nodes).toEqual(expected);
-    });
+    }, 15000);
 });

20533afd2f09380a6613fe2e03d5df172726fa42
Rachit Singh <rachitkrsingh@gmail.com>
2024-01-21 22:24:04 +0530

    Adding tailwind and related configs for styling

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

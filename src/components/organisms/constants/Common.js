222c16c9f6860b91ed739ca20a7317b70b9206d0
Rachit Singh <rachitkrsingh@gmail.com>
2024-03-04 01:39:36 +0530

    Removing unused directory and correcting the path for the same

--- src/components/organisms/constants/Common.js
+++
@@ -1,1 +0,0 @@
-export const FLOW_FILE_SUFFIX_REGEX = /^.+\.flow$/gm; // regex to check the file extension of flow files
--- src/components/organisms/constants/ImportCollectionTypes.js
+++
@@ -1,7 +0,0 @@
-const ImportCollectionTypes = {
-  YAML: 'yaml',
-  OPEN_API: 'open_api',
-  POSTMAN: 'postman',
-};
-
-export default ImportCollectionTypes;

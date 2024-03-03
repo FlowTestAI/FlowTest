9eba6c423d7e47521113b4a6381ad211739c0c27
Rachit Singh <rachitkrsingh@gmail.com>
2024-03-04 01:39:36 +0530

    Removing unused directory and correcting the path for the same

---
+++ src/constants/AppNavBar.js
@@ -0,0 +1,15 @@
+export const AppNavBarItems = {
+  // default value
+  collections: {
+    displayValue: 'Collections',
+    value: 'COLLECTIONS',
+  },
+  environments: {
+    displayValue: 'Environments',
+    value: 'ENVIRONMENTS',
+  },
+  history: {
+    displayValue: 'History',
+    value: 'HISTORY',
+  },
+};

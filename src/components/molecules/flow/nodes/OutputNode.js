d40a4c23c2088100b2e513e228f540ee97687687
Sajal Jain <jsajal1993@gmail.com>
2024-05-06 15:24:47 -0700

    display inital message in output node

--- src/components/molecules/flow/nodes/OutputNode.js
+++ src/components/molecules/flow/nodes/OutputNode.js
@@ -13,13 +13,17 @@ const OutputNode = ({ id, data }) => {
       handleRightData={{ type: 'source' }}
     >
       <div className='w-full text-xs text-gray-900 border border-gray-300 rounded-lg nodrag nowheel min-h-96 min-w-80 bg-gray-50 outline-blue-300 focus:border-blue-100 focus:ring-blue-100'>
-        <JsonEditor
-          name='output-text'
-          value={data.output ? JSON.stringify(data.output, null, 2) : 'Run flow to see data'}
-          placeholder='Run flow to see data'
-          readOnly={true}
-          maxLines={20}
-        />
+        {data.output ? (
+          <JsonEditor
+            name='output-text'
+            value={data.output ? JSON.stringify(data.output, null, 2) : 'Run flow to see data'}
+            placeholder='Run flow to see data'
+            readOnly={true}
+            maxLines={20}
+          />
+        ) : (
+          <div className='p-2'>{'Run flow to see data'}</div>
+        )}
       </div>
     </FlowNode>
   );
